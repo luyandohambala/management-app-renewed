89146cf15cae4a301b99f9f7e214fe8464433ba5
luyandohambala <luyandohambala.1@gmail.com>
2023-12-18 21:20:15 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace management_app_renewed
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
